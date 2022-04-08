24af10acd41ad7c40930099556012edb9311b5de
TheGodwill <sielle.sorho@gmail.com>
2022-04-07 22:49:03 -0700

    Level 0x00 Advanced pushing

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,8 @@
  */
 int main(void)
 {
-char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(1, ch, sizeof(ch));
-return (1);
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+	return (1);
 }
