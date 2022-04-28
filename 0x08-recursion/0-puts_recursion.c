1af1fa285c053956c6b12107f71dcc33281de6c2
TheGodwill <sielle.sorho@gmail.com>
2022-04-28 14:52:16 -0700

    C - Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
  * _puts_recursion - prints a string, followed by a new line
@@ -12,11 +12,12 @@ void _puts_recursion(char *s)
 
 if (*s == '\0')
 {
-_putchar('\n');
+	_putchar('\n');
 }
 else
 {
-_putchar(*s);
-_puts_recursion(s + 1);
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
 
+}
